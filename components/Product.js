568a8ecb4317b6ee6a3ba2ef08f2fee79aaf4f49
Amine13u <46264764+Amine13u@users.noreply.github.com>
2022-05-27 05:22:31 +0100

    Fixing the React.forwardRef() Warning

--- components/Product.js
+++ components/Product.js
@@ -6,7 +6,9 @@ const Product = ({ pizza }) => {
   return (
     <div className={styles.container}>
       <Link href={`/product/${pizza._id}`} passHref>
-        <Image src={pizza.image} width="500" height="500" />
+        <a>
+          <Image src={pizza.image} width="500" height="500" />
+        </a>
       </Link>
 
       <h1 className={styles.title}>{pizza.title}</h1>
